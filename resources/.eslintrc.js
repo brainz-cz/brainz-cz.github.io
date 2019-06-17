e583fa55d927e778d38fd37e96d4a0667d4c0830
Filip Novotný <filip@novotny.je>
2019-06-17 11:23:41 +0200

    Create .eslintrc.js

---
+++ resources/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": ["error"]
+  }
+}
